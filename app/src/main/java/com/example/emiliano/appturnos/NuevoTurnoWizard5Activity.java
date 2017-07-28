c57d0f6a74c9f96148106cf1e8bb45cb94fa4f9c
Emiliano <emiliano.sangoi@gmail.com>
2017-07-28 20:09:27 -0300

    Actividades 4 y 5 del formulario creadas

---
+++ app/src/main/java/com/example/emiliano/appturnos/NuevoTurnoWizard5Activity.java
@@ -0,0 +1,16 @@
+package com.example.emiliano.appturnos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * En esta actividad se revisan los parametros y se guarda el turno en el servidor.
+ */
+public class NuevoTurnoWizard5Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nuevo_turno_wizard5);
+    }
+}
